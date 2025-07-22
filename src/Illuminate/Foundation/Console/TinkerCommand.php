62aa8b1804acff6679bbd0f0144e8cef9091434a
Fisun Aleksandr <texnikru@gmail.com>
2025-07-22 11:27:35 +1000

    Revert "integrate Boris into Tinker command hwen available."
    This reverts commit 5e8d2c0e38b9630cf881336cbfde6c64657e4d30.
    
    # Conflicts:
    #	composer.json
    #	src/Illuminate/Foundation/Console/TinkerCommand.php

--- src/Illuminate/Foundation/Console/TinkerCommand.php
+++ src/Illuminate/Foundation/Console/TinkerCommand.php
@@ -1,6 +1,5 @@
 <?php namespace Illuminate\Foundation\Console;
 
-use Boris\Boris;
 use Illuminate\Console\Command;
 
 class TinkerCommand extends Command {
@@ -25,51 +24,6 @@ class TinkerCommand extends Command {
 	 * @return void
 	 */
 	public function fire()
-	{
-		if ($this->supportsBoris())
-		{
-			$this->runBorisShell();
-		}
-		else
-		{
-			$this->comment('Full REPL not supported. Falling back to simple shell.');
-
-			$this->runPlainShell();
-		}
-	}
-
-	/**
-	 * Run the Boris REPL with the current context.
-	 *
-	 * @return void
-	 */
-	protected function runBorisShell()
-	{
-		$this->setupBorisErrorHandling();
-
-		(new Boris('> '))->start();
-	}
-
-	/**
-	 * Setup the Boris exception handling.
-	 *
-	 * @return void
-	 */
-	protected function setupBorisErrorHandling()
-	{
-		restore_error_handler(); restore_exception_handler();
-
-		$this->laravel->make('artisan')->setCatchExceptions(false);
-
-		$this->laravel->error(function() { return ''; });
-	}
-
-	/**
-	 * Run the plain Artisan tinker shell.
-	 *
-	 * @return void
-	 */
-	protected function runPlainShell()
 	{
 		$input = $this->prompt();
 
@@ -114,16 +68,6 @@ class TinkerCommand extends Command {
 		$dialog = $this->getHelperSet()->get('dialog');
 
 		return $dialog->ask($this->output, "<info>></info>", null);
-	}
-
-	/**
-	 * Determine if the current environment supports Boris.
-	 *
-	 * @return bool
-	 */
-	protected function supportsBoris()
-	{
-		return extension_loaded('readline') && extension_loaded('posix') && extension_loaded('pcntl');
 	}
 
 }
